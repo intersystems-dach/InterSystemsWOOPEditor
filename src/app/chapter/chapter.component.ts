fa1b5f1b5e7add67e82df3f2caa8434a044e1b6a
phil1436 <philxsb@gmail.com>
2023-05-26 21:50:00 +0200

    update

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Chapter } from 'src/utils/classes';
+import { MarkdownService } from 'ngx-markdown';
+/* const { mdToPdf } = require('md-to-pdf'); */
 
+import pdfMake from 'pdfmake/build/pdfmake';
+
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+import htmlToPdfmake from 'html-to-pdfmake';
 @Component({
   selector: 'app-chapter',
   templateUrl: './chapter.component.html',
@@ -12,6 +21,8 @@ export class ChapterComponent {
 
   tipVisible: boolean = false;
   resultVisible: boolean = false;
+
+  constructor(private mdService: MarkdownService) {}
 
   showNextPage(): void {
     this.currentPage++;
@@ -33,5 +44,33 @@ export class ChapterComponent {
 
   showResult() {
     this.resultVisible = true;
+  }
+
+  async exportPDF() {
+    let content = '# ' + this.chapter.title + '\n---\n';
+    for (let page of this.chapter.pages) {
+      content += page.content + '\n---\n';
+      if (page.tip != '') {
+        content += '## TIP\n\n' + page.tip + '\n---\n';
+      }
+      if (page.result != '') {
+        content += '## RESULT\n\n' + page.result + '\n---\n';
+      }
+      content += '\n\n';
+    }
+    /* let pdf = await mdToPdf(
+      { content: content },
+      { dest: 'path/to/output.pdf' }
+    ); */
+    let html = this.mdService.parse(content);
+    console.log(html);
+    /* let logo =
+      '<img src="assets/imgs/InterSystemsWOOPLogo.png" width="100" height="100" />';
+    html = logo + html; */
+    let pdfContent = htmlToPdfmake(html);
+
+    const documentDefinition = { content: pdfContent };
+
+    pdfMake.createPdf(documentDefinition).open();
   }
 }
